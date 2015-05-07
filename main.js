c15b96e74e72b7d444ab026d6907b9a3577b5203
William Jimenez <william.jimenez@itsoninc.com>
2015-05-07 13:56:28 -0700

    first working version

---
+++ main.js
@@ -0,0 +1,54 @@
+/*
+  digitalocean-api-broker: a simple proxy to provide control over API requests to Digital Ocean
+  Written by: William Jimenez & Daniel Imfeld
+  Copyright (c) 2015
+
+  Based originally on proxy-http-to-https.js: Basic example of proxying over HTTP to a target HTTPS server
+  Copyright (c) Nodejitsu 2013
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var https = require('https'),
+    http  = require('http'),
+    util  = require('util'),
+    path  = require('path'),
+    fs    = require('fs'),
+    colors = require('colors'),
+    httpProxy = require('http-proxy');
+
+// Get our Digital Ocean API token from an environment variable
+var do_auth = process.env.DIGITALOCEAN_TOKEN
+if ( typeof do_auth == 'undefined' )
+{
+   console.log('Error: Digital Ocean environment variable not set'.red);
+   process.exit(1);
+}
+//
+// Create a HTTP Proxy server with a HTTPS target
+//
+httpProxy.createProxyServer({
+  target: 'https://api.digitalocean.com',
+  agent  : https.globalAgent,
+  headers: {
+    host: 'api.digitalocean.com',
+    Authorization: 'Bearer '+do_auth
+  }
+}).listen(5050);
+
+console.log('http proxy server'.blue + ' started '.green.bold + 'on port '.blue + '8011'.yellow);
+
